41e443cfc7437a867755d18ad98fe9daab36fc6a
Alicia <aliciafs1997@gmail.com>
2022-03-17 17:14:09 +0100

    pair programming python variables hecho

---
+++ pair programming python variables.py
@@ -0,0 +1,24 @@
+import math
+import random
+
+# Variables numéricas
+lim_superior = 6
+lim_inferior = 1
+
+
+# Cantidades calculadas
+intentos_permitidos = round(math.log(lim_superior - lim_inferior + 1.2))
+numero_secreto = random.randint(lim_inferior,lim_superior)
+
+# Inicio del juego
+x = "Bienvenidos al oraculo"
+print(x)
+
+y = "A ver si puedes adivinar el numero del 1 al 6"
+print(y)
+
+# Adivinar 1 intento
+print("Intento 1:" + input())
+
+# Fin del juego
+print("Y el numero es...", numero_secreto, "¡Espero que hayas acertado!")
