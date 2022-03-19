675842e578e5a4abf11363e861f5ead741d9f585
Alicia <aliciafs1997@gmail.com>
2022-03-19 19:53:33 +0100

    Pair Programming Python listas 2 hecho

--- pair programming python listas2.py
+++ pair programming python listas2.py
@@ -1,7 +1,7 @@
 import random
 
 # Variables numéricas
-index = -1
+index = 9
 lim_inferior = 0
 lim_superior = 20
 longitud_lista = 10
@@ -12,20 +12,22 @@ print("Esta lista aleatoria contiene", longitud_lista, "numeros entre el", lim_i
 
 # User input
 input("¿Cual crees que es?: ")
-a = input()
+numero_usuario = input()
 
 
 # Cantidades calculadas
 num_list = random.sample(range(lim_inferior, lim_superior), longitud_lista)
 
-# Inicio del juego
-
-# Propiedades de la lista aleatoria
-
 # Han adivinado un elemento?
+print("¿Has adivinado el numero?")
+bool_numero = numero_usuario in num_list
+print(bool_numero)
 
 # Si adivinaron uno, dónde estaba?
-if num_list.count(str_input): index = num_list.index(str_input)
+if num_list.count(numero_usuario): index = num_list.index(numero_usuario)
 if index >= 0: print("Estaba en el sitio", str(index+1) + ", índice", str(index)+".")
+
+print("Esta era la lista de numeros:", num_list)
+
 
 # Fin del juego
