888ca6a7629cd7b1c9a9396c26d88507ca3d7f74
Alicia <aliciafs1997@gmail.com>
2022-03-20 16:19:02 +0100

    pair programming pyhton lista 1 terminado

--- pair-programming-lista1.py
+++ pair-programming-lista1.py
@@ -3,21 +3,24 @@ import random
 x = "Bienvenidos al oraculo"
 print(x)
 
-str_input = "Escribe una lista de cinco numeros: " 
+#Cadenas fijas de texto
+str_input = "Escribe una lista de seis numeros: " 
 
+#Propiedades de la lista
 num_list = list(int(num) for num in input(str_input).strip().split())
 
 lim_inferior = min(num_list)
 lim_superior = max(num_list)
 
+#Cantidades calculadas
 print("El numero esta entre", lim_inferior, "y", lim_superior)
 numero_secreto = random.randint(lim_inferior,lim_superior)
 
-
-print("Y el numero es...", numero_secreto, "¡Espero que hayas acertado!")
-
+#¿Se ha adivinado el numero?
+print("¿Has adivinado el numero?")
 bool_borde_lista = numero_secreto in num_list
 print(bool_borde_lista)
 
-
+#Fin del juego
+print("Y el numero es...", numero_secreto, "¡Espero que hayas acertado!")
 
