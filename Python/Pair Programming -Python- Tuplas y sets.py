a542b0d7e16d1f9299e0905bd37002db8b7afda0
Alicia <aliciafs1997@gmail.com>
2022-03-27 19:41:27 +0200

    hecho

--- Python/Pair Programming -Python- Tuplas y sets.py
+++ Python/Pair Programming -Python- Tuplas y sets.py
@@ -9,29 +9,40 @@ print(bienvenida)
 
 # Imprimir tablero
 print(tablero[0][0] + " | " + tablero[0][1] + " | " + tablero[0][2])
-print(tablero[0][0] + " | " + tablero[0][1] + " | " + tablero[0][2])
-print(tablero[0][0] + " | " + tablero[0][1] + " | " + tablero[0][2])
-# Input de la usuaria
+print(tablero[1][0] + " | " + tablero[1][1] + " | " + tablero[1][2])
+print(tablero[2][0] + " | " + tablero[2][1] + " | " + tablero[2][2])
 
-jugador1 = input()
-coor = jugador1
-coor = tuple(int((num)-1 for num in coor.split(",")))
+# Input de la usuaria
+coor = input().split(" ")
+valor1 = coor[0]
+coordenadas = coor[1].split(",")
+#coor = tuple(int((num)-1 for num in coor.split(",")))
 
 # Actualizar tablero (siguiente paso)
+tablero[int(coordenadas[0])-1][int(coordenadas[1])-1] = valor1
 
-# Imprimir tablero
-
-# Input del usuario
-
+print(tablero[0][0] + " | " + tablero[0][1] + " | " + tablero[0][2])
+print(tablero[1][0] + " | " + tablero[1][1] + " | " + tablero[1][2])
+print(tablero[2][0] + " | " + tablero[2][1] + " | " + tablero[2][2])
 #coor = tuple(int(co)-1 for co in coor.split(","))
 
-# Actualizar tablero
+# Input de la usuaria
+coor = input().split(" ")
+valor2 = coor[0]
+coordenadas = coor[1].split(",")
 
-# Imprimir tablero
+# Actualizar tablero
+tablero[int(coordenadas[0])-1][int(coordenadas[1])-1] = valor2
+
+print(tablero[0][0] + " | " + tablero[0][1] + " | " + tablero[0][2])
+print(tablero[1][0] + " | " + tablero[1][1] + " | " + tablero[1][2])
+print(tablero[2][0] + " | " + tablero[2][1] + " | " + tablero[2][2])
+
 
 # Convertir los caracteres del board a una sola lista
 #board_list = sum(tablero,[])
 
 # Sacar los elementos únicos que no sean "-"
+print("Las usuarias han utilizado {",valor1,",",valor2,"}")
 
 # Final del juego
--- Python/pair programming python listas2.py
+++ Python/pair programming python listas2.py
@@ -1,26 +1,28 @@
 import random
 
 # Variables numéricas
-index = 9
+
 lim_inferior = 0
 lim_superior = 20
 longitud_lista = 10
+
 
 
 # Cadenas fijas de texto
 print("Esta lista aleatoria contiene", longitud_lista, "numeros entre el", lim_inferior, "y", lim_superior)
 
 # User input
-input("¿Cual crees que es?: ")
+print("¿Cual crees que es?")
 numero_usuario = input()
 
 
 # Cantidades calculadas
 num_list = random.sample(range(lim_inferior, lim_superior), longitud_lista)
+index = num_list.index(int(numero_usuario))
 
 # Han adivinado un elemento?
 print("¿Has adivinado el numero?")
-bool_numero = numero_usuario in num_list
+bool_numero = int(numero_usuario) in num_list
 print(bool_numero)
 
 # Si adivinaron uno, dónde estaba?
