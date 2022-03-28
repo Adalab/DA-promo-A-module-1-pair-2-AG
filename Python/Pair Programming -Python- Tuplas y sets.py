09c395b60e76b2db9cad9875143129d7de88f80c
Alicia <aliciafs1997@gmail.com>
2022-03-28 09:50:36 +0200

    revisado y terminado

--- Python/Pair Programming -Python- Tuplas y sets.py
+++ Python/Pair Programming -Python- Tuplas y sets.py
@@ -1,11 +1,11 @@
-# Variables predefinidas
-bienvenida = "Comencemos el juego del tres en raya"
+
+# Inicio del juego y explicacion a la usuaria
+print("Comencemos el juego del tres en raya")
+print("Para jugar debes elegir entre 'x' o 'o' y dos coordenadas entre 1 y 3")
 
 #Definir el tablero con todo "-"
 tablero = [["-", "-", "-"],["-", "-", "-"],["-", "-", "-"]]
 
-# Inicio del juego
-print(bienvenida)
 
 # Imprimir tablero
 print(tablero[0][0] + " | " + tablero[0][1] + " | " + tablero[0][2])
@@ -13,10 +13,10 @@ print(tablero[1][0] + " | " + tablero[1][1] + " | " + tablero[1][2])
 print(tablero[2][0] + " | " + tablero[2][1] + " | " + tablero[2][2])
 
 # Input de la usuaria
-coor = input().split(" ")
-valor1 = coor[0]
-coordenadas = coor[1].split(",")
-#coor = tuple(int((num)-1 for num in coor.split(",")))
+coor_usuaria= input().split(" ")
+valor1 = coor_usuaria[0]
+coordenadas = coor_usuaria[1].split(",")
+#coor = tuple(int((num)-1 for num in coor.split(","))) #Otra forma de hacerlo
 
 # Actualizar tablero (siguiente paso)
 tablero[int(coordenadas[0])-1][int(coordenadas[1])-1] = valor1
@@ -26,12 +26,12 @@ print(tablero[1][0] + " | " + tablero[1][1] + " | " + tablero[1][2])
 print(tablero[2][0] + " | " + tablero[2][1] + " | " + tablero[2][2])
 #coor = tuple(int(co)-1 for co in coor.split(","))
 
-# Input de la usuaria
+# Input de la usuaria 2
 coor = input().split(" ")
 valor2 = coor[0]
 coordenadas = coor[1].split(",")
 
-# Actualizar tablero
+# Actualizar tablero con el input de la usuaria 2
 tablero[int(coordenadas[0])-1][int(coordenadas[1])-1] = valor2
 
 print(tablero[0][0] + " | " + tablero[0][1] + " | " + tablero[0][2])
@@ -46,3 +46,4 @@ print(tablero[2][0] + " | " + tablero[2][1] + " | " + tablero[2][2])
 print("Las usuarias han utilizado {",valor1,",",valor2,"}")
 
 # Final del juego
+print("Gracias por participar")
