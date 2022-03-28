393537a18154893ae88139216726c62380280a67
Alicia <aliciafs1997@gmail.com>
2022-03-28 16:34:45 +0200

    actualizado

--- SQL/Pair programming consultas multiples 1.sql
+++ SQL/Pair programming consultas multiples 1.sql
@@ -1,4 +1,4 @@
-USE northwind ;
+USE Northwind;
 
 -- Ejercicio 1
 SELECT COUNT(orders.order_id), customers.company_name, customers.country
@@ -8,11 +8,15 @@ WHERE country = 'UK'
 GROUP BY customers.company_name ;
 
 -- Ejercicio 2
-SELECT customers.company_name, customers.country, COUNT(orders.order_id) AS Suma_Pedidos,  YEAR(orders.order_date)
-FROM orders 
+SELECT orderdetails.quantity, customers.company_name, customers.country, COUNT(orders.order_id) AS Suma_Pedidos, YEAR(orders.order_date)
+FROM orders
 INNER JOIN customers
+ON orders.customer_id = customers.customer_id
+INNER JOIN orderdetails
+ON orderdetails.order_id = orders.order_id
 WHERE country = 'UK'
-GROUP BY company_name ;
+GROUP BY order_date
+ORDER BY order_date;
 
 -- Ejercicio 3
 SELECT customers.company_name, SUM(products.unit_price), SUM(products.unit_price*0.85), customers.country, COUNT(orders.order_id) AS Suma_Pedidos,  YEAR(orders.order_date)
@@ -27,3 +31,5 @@ FROM orders
 INNER JOIN customers
 WHERE country = 'UK'
 GROUP BY company_name ;
+
+-- Ejercicio 5
