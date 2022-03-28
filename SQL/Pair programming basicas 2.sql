e7a9c7953f5e7706847524390f3e6c1c14c2aa4d
Alicia <aliciafs1997@gmail.com>
2022-03-28 16:57:13 +0200

    terminado

--- SQL/Pair programming basicas 2.sql
+++ SQL/Pair programming basicas 2.sql
@@ -1,60 +1,60 @@
-USE northwind;
+USE Northwind;
 
 /*ejercicio 1*/
-SELECT ProductName, UnitPrice
+SELECT product_name, unit_price
 FROM products
-ORDER BY productID
+ORDER BY product_id
 LIMIT 10 ; 
 
 /*Ejercicio 2*/
-SELECT ProductName, UnitPrice
+SELECT product_name, unit_price
 FROM products
-ORDER BY productID DESC
+ORDER BY product_id DESC
 LIMIT 10 ;
 
 /*Ejercicio 3*/
-SELECT DISTINcT OrderID
+SELECT DISTINcT order_id
 FROM orderdetails ; 
 
 /*Ejercicio 4*/
 SELECT *
 FROM orderdetails
-WHERE ProductID BETWEEN 1 AND 2
+WHERE product_id BETWEEN 1 AND 2
 LIMIT 2 ;
 
 /*Ejercicio 5*/
-SELECT UnitPrice AS ImporteTotal, productID
+SELECT unit_price AS importeTotal, product_id
 FROM orderdetails
-ORDER BY UnitPrice DESC
+ORDER BY unit_price DESC
 LIMIT 3 ;
 
 /*Ejercicio 6*/
-SELECT Unitprice
+SELECT unit_price
 FROM orderdetails
-WHERE UnitPrice BETWEEN 5 AND 10
-ORDER BY OrderID DESC
+WHERE unit_price BETWEEN 5 AND 10
+ORDER BY order_id DESC
 LIMIT 5 ;
 
 /*Ejercicio 7*/
-SELECT ProductName AS NombreDeCategoria
+SELECT product_name AS NombreDeCategoria
 FROM products
-ORDER BY productID ; 
+ORDER BY product_id ; 
 
 /*Ejercicio 8*/
-SELECT ShippedDate, 
-OrderDate, 
-OrderID,
-DATE_ADD(ShippedDate, INTERVAL 5 DAY) AS FechaRetrasada,
-RequiredDate
+SELECT shipped_date, 
+order_date, 
+order_id,
+DATE_ADD(shipped_date, INTERVAL 5 DAY) AS FechaRetrasada,
+required_date
 FROM orders
 LIMIT 15 ; 
 
 /*Ejercicio 9*/
-SELECT ProductID, UnitPrice
+SELECT product_id, unit_price
 FROM orderdetails
-WHERE UnitPrice BETWEEN 15 AND 50 ; 
+WHERE unit_price BETWEEN 15 AND 50 ; 
 
 /*Ejercicio 10*/
-SELECT ProductID, UnitPrice
+SELECT product_id, unit_price
 FROM orderdetails
-WHERE UnitPrice IN (18, 19, 20) ;
+WHERE unit_price IN (18, 19, 20) ;
