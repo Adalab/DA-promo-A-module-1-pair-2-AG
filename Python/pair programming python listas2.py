b1b35208891b0ce0a4b1fcf244558fc64195f3ac
Alicia <aliciafs1997@gmail.com>
2022-03-28 10:22:05 +0200

    revisado y terminado

--- Python/pair programming python listas2.py
+++ Python/pair programming python listas2.py
@@ -1,11 +1,10 @@
 import random
 
 # Variables numéricas
-
-lim_inferior = 0
+index = -1
+lim_inferior = 1
 lim_superior = 20
 longitud_lista = 10
-
 
 
 # Cadenas fijas de texto
@@ -13,23 +12,26 @@ print("Esta lista aleatoria contiene", longitud_lista, "numeros entre el", lim_i
 
 # User input
 print("¿Cual crees que es?")
-numero_usuario = input()
+numero_usuario = int(input())
 
 
 # Cantidades calculadas
 num_list = random.sample(range(lim_inferior, lim_superior), longitud_lista)
-index = num_list.index(int(numero_usuario))
 
-# Han adivinado un elemento?
+
+# ¿Han adivinado un elemento?
 print("¿Has adivinado el numero?")
-bool_numero = int(numero_usuario) in num_list
+bool_numero = bool(int(numero_usuario) in num_list)
 print(bool_numero)
 
-# Si adivinaron uno, dónde estaba?
-if num_list.count(numero_usuario): index = num_list.index(numero_usuario)
-if index >= 0: print("Estaba en el sitio", str(index+1) + ", índice", str(index)+".")
 
-print("Esta era la lista de numeros:", num_list)
+# Si adivinaron, y donde estaba
+if num_list.count(numero_usuario): 
+    index = num_list.index(numero_usuario)
+
+if index >= 0: 
+    print("Estaba en el sitio", str(index+1) + ", índice", str(index)+".")
 
 
 # Fin del juego
+print("Esta era la lista de numeros:", num_list)
